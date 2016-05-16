edb2898f4b80b4bc9a57f02a7fce3f83fef57ba3
camelia_sh <camelia0858@gmail.com>
2016-05-16 18:13:00 +0900

    남도학숙 식단표 크롤러

---
+++ NDHS_Crawler.py
@@ -0,0 +1,41 @@
+#-*- coding: utf-8 -*-
+import scrapy
+from scrapy.crawler import CrawlerProcess
+
+
+class MyCrawler(scrapy.Spider):
+    name = 'NDHS_CRAWLER'
+    start_urls = ['http://www.ndhs.or.kr/2014/sub_community/sub2.php']
+
+    def parse(self, response):
+        l = response.css("div > div > div > div > div > div > table > tbody > tr > th").extract()
+        m = response.css("div > div > div > div > div > div > table > tbody > tr > td").extract()
+
+        for i in range(len(l)):
+            if i == 5 :
+                l[i] = l[i][40:-12]
+            elif i == 6:
+                l[i] = l[i][38:-12]
+            else:
+                l[i] = l[i][16:-5]
+
+        for i in range(len(m)):
+            m[i] = m[i][4:-5]
+
+        for i in range(7):
+            print l[i]
+            print "조식 : ",
+            print m[3*i + 0]
+            print "중식 : ",
+            print m[3*i + 1]
+            print "석식 : ",
+            print m[3*i + 2 ]
+
+    pass
+
+process = CrawlerProcess({
+    'USER_AGENT': 'Mozilla/4.0 (compatible; MSIE 7.0; Windows NT 5.1)'
+})
+
+process.crawl(MyCrawler)
+process.start()

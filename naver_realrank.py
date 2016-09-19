de37e8aa404c01622123fc74ee88ebbabb7d67e5
Camelia <camelia0858@gmail.com>
2016-09-20 03:31:55 +0900

    modified fit in settings.json form

--- naver_realrank.py
+++ naver_realrank.py
@@ -5,19 +5,21 @@ from scrapy.crawler import CrawlerProcess
 class NaverRealRankCrawler(scrapy.Spider):
     name = 'naver realrank'
     start_urls = ['http://www.naver.com']
-
+    base_urls = "http://search.naver.com/search.naver?where=nexearch&query="
     def parse(self, response):
         query_real_rank = '#realrank'
-        print response.css(query_real_rank).extract()
+#       print response.css(query_real_rank).extract()
 
         titles = response.css('#realrank > li > a::attr(title)').extract()
         urls = response.css('#realrank > li > a::attr(href)').extract()
 
         results = zip(titles, urls)
 
+        i=1
         for title, url in results:
-            print title, url
-
+            if i <=10:
+                print(str(i) + "&^%987&^%" + title + "&^%987&^%" + self.base_urls + title)
+            i+=1
 process = CrawlerProcess({
     'USER_AGENT': 'Mozilla/4.0 (compatible; MSIE 7.0; Windows NT 5.1)'
 })
